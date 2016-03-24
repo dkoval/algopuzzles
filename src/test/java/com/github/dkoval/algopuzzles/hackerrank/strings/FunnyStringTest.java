b57571f0000f5bafc2f43b29d8c986f65565af22
dkoval <koval.dmitriy@gmail.com>
2016-03-24 23:08:09 +0100

    add solution to HackerRank's `Funny String` problem https://www.hackerrank.com/challenges/funny-string

---
+++ src/test/java/com/github/dkoval/algopuzzles/hackerrank/strings/FunnyStringTest.java
@@ -0,0 +1,38 @@
+package com.github.dkoval.algopuzzles.hackerrank.strings;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link FunnyString} solution.
+ */
+@RunWith(Parameterized.class)
+public class FunnyStringTest {
+    private final String input;
+    private final boolean isFunny;
+
+    public FunnyStringTest(String input, boolean isFunny) {
+        this.input = input;
+        this.isFunny = isFunny;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"acxz", true},
+                {"bcxz", false},
+        });
+    }
+
+    @Test
+    public void testIsFunny() throws Exception {
+        boolean answer = FunnyString.isFunny(input);
+        assertEquals(isFunny, answer);
+    }
+}
