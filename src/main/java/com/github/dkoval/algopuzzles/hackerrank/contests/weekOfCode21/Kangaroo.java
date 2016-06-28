88a69a9d63a509f07b653ac360bb0bc75761c3dd
dkoval <koval.dmitriy@gmail.com>
2016-06-28 09:13:00 +0200

    HackerRank Week of Code 21: solved `Kangaroo` problem https://www.hackerrank.com/contests/w21/challenges/kangaroo

---
+++ src/main/java/com/github/dkoval/algopuzzles/hackerrank/contests/weekOfCode21/Kangaroo.java
@@ -0,0 +1,24 @@
+package com.github.dkoval.algopuzzles.hackerrank.contests.weekOfCode21;
+
+import java.util.Scanner;
+
+/**
+ * <a href="https://www.hackerrank.com/contests/w21/challenges/kangaroo">Problem:</a>
+ * Max Score: 10 Difficulty: Easy
+ */
+public class Kangaroo {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        boolean answer = solve(x1, v1, x2, v2);
+        System.out.println(answer ? "YES" : "NO");
+    }
+
+    static boolean solve(int x1, int v1, int x2, int v2) {
+        return (x2 > x1) && (v1 > v2) && ((x2 - x1) % (v1 - v2) == 0);
+    }
+}
---
+++ src/test/java/com/github/dkoval/algopuzzles/hackerrank/contests/weekOfCode21/KangarooTest.java
@@ -0,0 +1,44 @@
+package com.github.dkoval.algopuzzles.hackerrank.contests.weekOfCode21;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link Kangaroo} solution.
+ */
+@RunWith(Parameterized.class)
+public class KangarooTest {
+    private final int x1;
+    private final int v1;
+    private final int x2;
+    private final int v2;
+    private final boolean answer;
+
+    public KangarooTest(int x1, int v1, int x2, int v2, boolean answer) {
+        this.x1 = x1;
+        this.v1 = v1;
+        this.x2 = x2;
+        this.v2 = v2;
+        this.answer = answer;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0, 3, 4, 2, true},
+                {0, 2, 5, 3, false},
+        });
+    }
+
+    @Test
+    public void solution() throws Exception {
+        boolean myAnswer = Kangaroo.solve(x1, v1, x2, v2);
+        assertEquals(answer, myAnswer);
+    }
+}
