e3742a984ed82b6d5e9638a297936974d8af7d71
dkoval <koval.dmitriy@gmail.com>
2016-07-17 19:42:56 +0200

    CtCI - added solution to `Route Between Nodes` question

---
+++ ctci/src/main/java/com/github/dkoval/algopuzzles/ctci/ch04TreesAndGraphs/Q01RouteBetweenNodes.java
@@ -0,0 +1,71 @@
+package com.github.dkoval.algopuzzles.ctci.ch04TreesAndGraphs;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Q4.1 Route Between Nodes: Give a directed graph, design an algorithm to find out whether there is a
+ * route between two nodes.
+ */
+public class Q01RouteBetweenNodes {
+
+    public static class Graph {
+        private final List<Edge> adj[];
+
+        public Graph(int n) {
+            adj = (List<Edge>[]) new List[n];
+            for (int i = 0; i < n; i++) {
+                adj[i] = new LinkedList<>();
+            }
+        }
+
+        public void add(Edge e) {
+            int v = e.from;
+            adj[v].add(e);
+        }
+
+        public Iterable<Edge> adj(int v) {
+            return adj[v];
+        }
+
+        public int numOfVertices() {
+            return adj.length;
+        }
+    }
+
+    public static class Edge {
+        private final int from;
+        private final int to;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    public static boolean search(Graph g, int start, int end) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean visited[] = new boolean[g.numOfVertices()];
+        // BFS
+        visited[start] = true;
+        q.add(start);
+        while (!q.isEmpty()) {
+            int u = q.remove();
+            if (u == end) {
+                // there is a path
+                return true;
+            }
+            for (Edge e : g.adj(u)) {
+                int v = e.to;
+                if (!visited[v]) {
+                    // only consider vertices that have not yet been visited
+                    visited[v] = true;
+                    q.add(v);
+                }
+            }
+            visited[u] = true;
+        }
+        return false;
+    }
+}
---
+++ ctci/src/test/java/com/github/dkoval/algopuzzles/ctci/ch04TreesAndGraphs/Q01RouteBetweenNodesTest.java
@@ -0,0 +1,51 @@
+package com.github.dkoval.algopuzzles.ctci.ch04TreesAndGraphs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link Q01RouteBetweenNodes} solution.
+ */
+@RunWith(Parameterized.class)
+public class Q01RouteBetweenNodesTest {
+    private final Q01RouteBetweenNodes.Graph g;
+    private final int start;
+    private final int end;
+    private final boolean answer;
+
+    public Q01RouteBetweenNodesTest(Q01RouteBetweenNodes.Graph g, int start, int end, boolean answer) {
+        this.g = g;
+        this.start = start;
+        this.end = end;
+        this.answer = answer;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {createTestGraph(), 3, 5, true},
+        });
+    }
+
+    private static Q01RouteBetweenNodes.Graph createTestGraph() {
+        Q01RouteBetweenNodes.Graph g = new Q01RouteBetweenNodes.Graph(6);
+        g.add(new Q01RouteBetweenNodes.Edge(0, 1));
+        g.add(new Q01RouteBetweenNodes.Edge(0, 2));
+        g.add(new Q01RouteBetweenNodes.Edge(0, 3));
+        g.add(new Q01RouteBetweenNodes.Edge(3, 4));
+        g.add(new Q01RouteBetweenNodes.Edge(4, 5));
+        return g;
+    }
+
+    @Test
+    public void solution() {
+        boolean myAnswer = Q01RouteBetweenNodes.search(g, start, end);
+        assertEquals(answer, myAnswer);
+    }
+}
