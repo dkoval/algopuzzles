61b07bb221be2c2561863e07eac4c5c0d154bebf
dkoval <koval.dmitriy@gmail.com>
2016-08-21 23:52:58 +0200

    added solution to Firecode.io `Anagrams` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/Anagrams.java
@@ -0,0 +1,26 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+/**
+ * Write a method {@link #isAnagram(String, String)} that checks if two lowercase input Strings are anagrams of each other.
+ * An anagram of a String is a String that is formed by simply re-arranging its letters, using each letter
+ * exactly once. Your algorithm should run in linear O(n) time and use constant O(1) space.
+ */
+public class Anagrams {
+
+    public static boolean isAnagram(String input1, String input2) {
+        if (input1 == null || input2 == null || input1.length() != input2.length()) {
+            return false;
+        }
+        int[] freq = new int[26];
+        for (int i = 0; i < input1.length(); i++) {
+            freq[input1.charAt(i) - 'a']++;
+            freq[input2.charAt(i) - 'a']--;
+        }
+        for (int f : freq) {
+            if (f != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/AnagramsTest.java
@@ -0,0 +1,46 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link Anagrams} solution.
+ */
+@RunWith(Parameterized.class)
+public class AnagramsTest {
+    private final String input1;
+    private final String input2;
+    private final boolean expected;
+
+    public AnagramsTest(String input1, String input2, boolean expected) {
+        this.input1 = input1;
+        this.input2 = input2;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"abc", "cab", true},
+                {"b", "b", true},
+                {"bd", "cb", false},
+                {"hello", "hi", false},
+                {"yellow", "llowey", true},
+                {"h", "h", true},
+                {"abcde", "acdbe", true},
+                {null, null, false},
+        });
+    }
+
+    @Test
+    public void isAnagram() throws Exception {
+        boolean actual = Anagrams.isAnagram(input1, input2);
+        assertEquals(expected, actual);
+    }
+}
