5fa337a5d82847dc2fa23d5cfe0fa591966f1bdf
dkoval <koval.dmitriy@gmail.com>
2016-08-21 23:00:39 +0200

    added solution to Firecode.io `BinaryRepresentation` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/BinaryRepresentation.java
@@ -0,0 +1,21 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+/**
+ * Write a method to compute the binary representation of a positive integer.
+ * The method should return a string with 1s and 0s.
+ */
+public class BinaryRepresentation {
+
+    public static String computeBinary(int val) {
+        if (val == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        while (val > 0) {
+            int binDigit = val % 2;
+            sb.append((char)('0' + binDigit));
+            val /= 2;
+        }
+        return sb.reverse().toString();
+    }
+}
---
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/BinaryRepresentationTest.java
@@ -0,0 +1,41 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link BinaryRepresentation} solution.
+ */
+@RunWith(Parameterized.class)
+public class BinaryRepresentationTest {
+    private final int input;
+    private final String expected;
+
+    public BinaryRepresentationTest(int input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {15, "1111"},
+                {10, "1010"},
+                {52, "110100"},
+                {0, "0"},
+                {1, "1"},
+        });
+    }
+
+    @Test
+    public void computeBinary() throws Exception {
+        String actual = BinaryRepresentation.computeBinary(input);
+        assertEquals(expected, actual);
+    }
+}
