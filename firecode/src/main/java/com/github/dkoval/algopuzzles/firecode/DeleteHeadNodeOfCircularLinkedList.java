e215a3df7e2b857b0194810c3adccf64a9178002
dkoval <koval.dmitriy@gmail.com>
2016-08-23 23:50:09 +0200

    added solution to Firecode.io `Delete the Head Node of a Circular Linked List ` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/DeleteHeadNodeOfCircularLinkedList.java
@@ -0,0 +1,26 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.ListNode;
+
+/**
+ * Given a circular linked list, implement a method to delete its head node. Return the list's new head node.
+ * <pre>
+ * *x = indicates head node
+ * 1->2->3->4->*1 ==> 2->3->4->*2
+ * </pre>
+ */
+public class DeleteHeadNodeOfCircularLinkedList {
+
+    public static ListNode deleteAtHead(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode curr = head;
+        while (curr.next != head) {
+            curr = curr.next;
+        }
+        curr.next = head.next;
+        head.next = null;
+        return curr.next;
+    }
+}
---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/lib/ListNode.java
@@ -0,0 +1,10 @@
+package com.github.dkoval.algopuzzles.firecode.lib;
+
+public class ListNode {
+    public int data;
+    public ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
