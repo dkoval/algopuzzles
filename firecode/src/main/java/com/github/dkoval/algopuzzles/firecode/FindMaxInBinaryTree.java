ad76ec8bc634f255b93aa80063f3e6822516504e
dkoval <koval.dmitriy@gmail.com>
2016-08-23 23:07:33 +0200

    added solution to Firecode.io `Find the Max Element in a Binary Tree Recursively` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/FindMaxInBinaryTree.java
@@ -0,0 +1,16 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+
+/**
+ * Given a binary tree, write a recursive method to return the maximum element.
+ */
+public class FindMaxInBinaryTree {
+
+    public static int findMax(TreeNode root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        return Math.max(root.data, Math.max(findMax(root.left), findMax(root.right)));
+    }
+}
---
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/FindMaxInBinaryTreeTest.java
@@ -0,0 +1,54 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link FindMaxInBinaryTree} solution.
+ */
+@RunWith(Parameterized.class)
+public class FindMaxInBinaryTreeTest {
+    private final TreeNode root;
+    private final int expected;
+
+    public FindMaxInBinaryTreeTest(TreeNode root, int expected) {
+        this.root = root;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new TreeNode(4, null, null), 4},
+                {new TreeNode(4,
+                        new TreeNode(40,
+                                new TreeNode(41, null, null), null),
+                        new TreeNode(45, null, null)), 45},
+                {new TreeNode(6,
+                        new TreeNode(9,
+                                new TreeNode(5, null, null),
+                                new TreeNode(8, null, null)),
+                        new TreeNode(4, null, null)), 9},
+                {new TreeNode(1,
+                        new TreeNode(2,
+                                new TreeNode(4, null, null),
+                                new TreeNode(5, null, null)),
+                        new TreeNode(3,
+                                new TreeNode(6, null, null),
+                                new TreeNode(7, null, null))), 7},
+        });
+    }
+
+    @Test
+    public void findMax() throws Exception {
+        int actual = FindMaxInBinaryTree.findMax(root);
+        assertEquals(expected, actual);
+    }
+}
