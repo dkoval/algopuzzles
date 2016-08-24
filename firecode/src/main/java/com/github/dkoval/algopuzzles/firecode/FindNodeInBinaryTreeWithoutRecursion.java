008745e41a7604ebf0fbc71a38b97604a321ba69
dkoval <koval.dmitriy@gmail.com>
2016-08-25 00:36:12 +0200

    added solution to Firecode.io `Find a Node in a Binary Tree Without Using Recursion` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/FindNodeInBinaryTreeWithoutRecursion.java
@@ -0,0 +1,34 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, write a method to find and return the node with data = the input data.
+ * Do not use recursion.
+ */
+public class FindNodeInBinaryTreeWithoutRecursion {
+
+    public static TreeNode findNode(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode n = q.remove();
+            if (n.data == val) {
+                return n;
+            }
+            if (n.left != null) {
+                q.add(n.left);
+            }
+            if (n.right != null) {
+                q.add(n.right);
+            }
+        }
+        return null;
+    }
+}
---
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/FindNodeInBinaryTreeWithoutRecursionTest.java
@@ -0,0 +1,49 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests {@link FindNodeInBinaryTreeWithoutRecursion} solution.
+ */
+@RunWith(Parameterized.class)
+public class FindNodeInBinaryTreeWithoutRecursionTest {
+    private final TreeNode root;
+    private final int val;
+    private final Integer expected;
+
+    public FindNodeInBinaryTreeWithoutRecursionTest(TreeNode root, int val, Integer expected) {
+        this.root = root;
+        this.val = val;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {null, 1, null},
+                {new TreeNode(1, null, null), 2, null},
+                {new TreeNode(6,
+                        new TreeNode(4,
+                                new TreeNode(3, null, null),
+                                new TreeNode(5, null, null)),
+                        new TreeNode(8,
+                                null,
+                                new TreeNode(9, null, null))), 4, 4},
+        });
+    }
+
+
+    @Test
+    public void findNode() throws Exception {
+        TreeNode actual = FindNodeInBinaryTreeWithoutRecursion.findNode(root, val);
+        assertEquals(expected, (actual != null) ? actual.data : null);
+    }
+}
