e81c2ff1955dd9b64a1bdfd141c16f61ecedbfce
dkoval <koval.dmitriy@gmail.com>
2016-08-24 23:52:08 +0200

    added solution to Firecode.io `Iterative Preorder Traversal` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/IterativePreorderTraversalOfBinaryTree.java
@@ -0,0 +1,35 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+/**
+ * Given a binary tree, write a method to iteratively traverse the tree in the preorder manner.
+ * Mark a node as visited by adding its data to a list - Arraylist<Integer> preorderedList.
+ * Return this list.
+ */
+public class IterativePreorderTraversalOfBinaryTree {
+
+    public static ArrayList<Integer> preorderItr(TreeNode root) {
+        ArrayList<Integer> preorderedList = new ArrayList<>();
+        if (root == null) {
+            return preorderedList;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode top = stack.pop();
+            preorderedList.add(top.data);
+            if (top.right != null) {
+                stack.push(top.right);
+            }
+            if (top.left != null) {
+                stack.push(top.left);
+            }
+        }
+        return preorderedList;
+    }
+}
---
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/IterativePreorderTraversalOfBinaryTreeTest.java
@@ -0,0 +1,57 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+import com.github.dkoval.algopuzzles.firecode.lib.TreeNode;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Tests {@link IterativePreorderTraversalOfBinaryTree} solution.
+ */
+@RunWith(Parameterized.class)
+public class IterativePreorderTraversalOfBinaryTreeTest {
+    private final TreeNode root;
+    private final List<Integer> expected;
+
+    public IterativePreorderTraversalOfBinaryTreeTest(TreeNode root, List<Integer> expected) {
+        this.root = root;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {null, Collections.emptyList()},
+                {new TreeNode(4, null, null), Collections.singletonList(4)},
+                {new TreeNode(4, new TreeNode(6, null, null), null), Arrays.asList(4, 6)},
+                {new TreeNode(6,
+                        new TreeNode(4,
+                                new TreeNode(3, null, null),
+                                null),
+                        new TreeNode(8,
+                                null,
+                                new TreeNode(9, null, null))), Arrays.asList(6, 4, 3, 8, 9)},
+                {new TreeNode(1,
+                        new TreeNode(2,
+                                new TreeNode(4, null, null),
+                                new TreeNode(5, null, null)),
+                        new TreeNode(3,
+                                new TreeNode(6, null, null),
+                                new TreeNode(7, null, null))), Arrays.asList(1, 2, 4, 5, 3, 6, 7)},
+        });
+    }
+
+    @Test
+    public void preorderItr() throws Exception {
+        List<Integer> actual = IterativePreorderTraversalOfBinaryTree.preorderItr(root);
+        assertThat(actual, is(expected));
+    }
+}
