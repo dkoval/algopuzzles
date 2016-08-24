216db191fc6760550b2fa07bc3cb2b0e2a57d822
dkoval <koval.dmitriy@gmail.com>
2016-08-24 22:57:19 +0200

    added solution to Firecode.io `Find the Transpose of a Square Matrix` problem

---
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/TransposeOfSquareMatrix.java
@@ -0,0 +1,18 @@
+package com.github.dkoval.algopuzzles.firecode;
+
+/**
+ * You are given a square 2D image matrix where each integer represents a pixel.
+ * Write a method transposeMatrix to transform the matrix into its Transpose - in-place.
+ */
+public class TransposeOfSquareMatrix {
+
+    public static void transposeMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
