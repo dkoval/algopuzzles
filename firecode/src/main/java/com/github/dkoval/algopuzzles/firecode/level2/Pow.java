3dd25d2d06655a37d044df900ce3180b37766fb9
dkoval <koval.dmitriy@gmail.com>
2016-08-31 08:44:56 +0200

    added recursive and iterative solutions to Firecode.io `Pow` problem

--- firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/level2/Pow.java
+++ firecode/src/main/java/com/github/dkoval/algopuzzles/firecode/level2/Pow.java
@@ -1,23 +1,41 @@
 package com.github.dkoval.algopuzzles.firecode.level2;
 
 /**
- * Write a method - pow(x,n) that returns the value of x raised to the power of n (x^n).
+ * Write a method - powRecursive(x,n) that returns the value of x raised to the power of n (x^n).
  * n can be negative!
  */
 public class Pow {
 
-    public static double pow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
+    public static double powRecursive(double x, int n) {
         if (Double.compare(x, 0d) == 0) {
             return 0d;
         }
-        double half = pow(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return (n > 0) ? half * half * x : half * half / x;
+        return (n < 0) ? 1 / doPowRecursive(x, -n) : doPowRecursive(x, n);
+    }
+
+    public static double powIterative(double x, int n) {
+        if (Double.compare(x, 0d) == 0) {
+            return 0d;
         }
+        return (n < 0) ? 1 / doPowIterative(x, -n) : doPowIterative(x, n);
+    }
+
+    /* n is expected to be a non-negative number here */
+    private static double doPowRecursive(double x, int n) {
+        if (n == 0) {
+            return 1d;
+        }
+        double half = doPowRecursive(x, n / 2);
+        return (n % 2 == 0) ? half * half : half * half * x;
+    }
+
+    /* n is expected to be a non-negative number here */
+    private static double doPowIterative(double x, int n) {
+        double p = (n % 2 == 0) ? 1d : x;
+        while (n > 1) {
+            p *= x * x;
+            n /= 2;
+        }
+        return p;
     }
 }
--- firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/level2/PowTest.java
+++ firecode/src/test/java/com/github/dkoval/algopuzzles/firecode/level2/PowTest.java
@@ -27,22 +27,40 @@ public class PowTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {1.0, 4, 1.0},
+                {1.0, 1, 1.0},
+                {1.0, 2, 1.0},
+                {1.0, 3, 1.0},
+                {1.0, -1, 1.0},
+                {1.0, -2, 1.0},
+                {1.0, -3, 1.0},
                 {2.0, 0, 1.0},
                 {2.0, 1, 2.0},
+                {2.0, 2, 4.0},
                 {2.0, 3, 8.0},
                 {2.0, -1, 0.5},
                 {2.0, -2, 0.25},
+                {2.0, -3, 0.125},
                 {-2.0, 0, 1.0},
+                {-2.0, 1, -2.0},
                 {-2.0, 2, 4.0},
+                {-2.0, 3, -8.0},
+                {-2.0, -1, -0.5},
+                {-2.0, -2, 0.25},
+                {-2.0, -3, -0.125},
                 {0.0, Integer.MAX_VALUE, 0.0},
                 {0.0, Integer.MIN_VALUE, 0.0},
         });
     }
 
     @Test
-    public void pow() throws Exception {
-        double actual = Pow.pow(x, n);
+    public void powRecursive() throws Exception {
+        double actual = Pow.powRecursive(x, n);
+        assertEquals(expected, actual, 1E-6);
+    }
+
+    @Test
+    public void powIterative() throws Exception {
+        double actual = Pow.powIterative(x, n);
         assertEquals(expected, actual, 1E-6);
     }
 }
