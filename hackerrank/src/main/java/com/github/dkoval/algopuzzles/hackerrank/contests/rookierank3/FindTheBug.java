34ed6bac96ddbd47c445767e1ef224e249450bc2
dkoval <koval.dmitriy@gmail.com>
2017-05-06 20:47:51 +0200

    added solution to RookieRank 3 :: Find the Bug https://www.hackerrank.com/contests/rookierank-3/challenges/find-the-bug

---
+++ hackerrank/src/main/java/com/github/dkoval/algopuzzles/hackerrank/contests/rookierank3/FindTheBug.java
@@ -0,0 +1,36 @@
+package com.github.dkoval.algopuzzles.hackerrank.contests.rookierank3;
+
+import java.util.Scanner;
+
+/**
+ * <a href="https://www.hackerrank.com/contests/rookierank-3/challenges/find-the-bug">Problem:</a>
+ * Max Score: 10 Difficulty: Easy
+ */
+public class FindTheBug {
+
+    static int[] findTheBug(String[] grid){
+        for (int i = 0; i < grid.length; i++) {
+            String row = grid[i];
+            int j = row.indexOf('X');
+            if (j != -1) {
+                return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String[] grid = new String[n];
+        for(int grid_i=0; grid_i < n; grid_i++){
+            grid[grid_i] = in.next();
+        }
+        // Return an array containing [r, c]
+        int[] result = findTheBug(grid);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "," : ""));
+        }
+        System.out.println("");
+    }
+}
