27a285d5660f11837fe143564173b87de17c42d6
dkoval <koval.dmitriy@gmail.com>
2017-07-30 17:13:47 +0200

    minor improvements

--- hackerrank/src/main/java/com/github/dkoval/algopuzzles/hackerrank/tutorials/ctci/BalancedBrackets.java
+++ hackerrank/src/main/java/com/github/dkoval/algopuzzles/hackerrank/tutorials/ctci/BalancedBrackets.java
@@ -1,26 +1,19 @@
 package com.github.dkoval.algopuzzles.hackerrank.tutorials.ctci;
 
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 /**
  * <a href="https://www.hackerrank.com/challenges/ctci-balanced-brackets">Stacks: Balanced Brackets</a>
  * Max Score: 30 Difficulty: Medium
  */
 public class BalancedBrackets {
-    private static final Set<Character> openingBrackets;
-    private static final Set<Character> closingBrackets;
-
-    static {
-        openingBrackets = new HashSet<>();
-        openingBrackets.add('(');
-        openingBrackets.add('[');
-        openingBrackets.add('{');
-
-        closingBrackets = new HashSet<>();
-        closingBrackets.add(')');
-        closingBrackets.add(']');
-        closingBrackets.add('}');
-    }
+    private static final char[][] brackets = {
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+    };
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -32,29 +25,35 @@ public class BalancedBrackets {
     }
 
     static boolean isBalanced(String expression) {
-        Deque<Character> leftChars = new LinkedList<>();
+        Deque<Character> stack = new LinkedList<>();
         for (int i = 0; i < expression.length(); i++) {
             char ch = expression.charAt(i);
-            if (openingBrackets.contains(ch)) {
-                leftChars.push(ch);
-            }
-            if (closingBrackets.contains(ch)) {
-                if (leftChars.isEmpty()) {
-                    // unmatched closing bracket
-                    return false;
-                }
-                if (!isMatchingPairOfBrackets(leftChars.pop(), ch)) {
-                    // mismatched pair of brackets
+            if (isOpeningBracket(ch)) {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty() || !isMatchingPairOfBrackets(stack.pop(), ch)) {
                     return false;
                 }
             }
         }
-        return leftChars.isEmpty();
+        return stack.isEmpty();
+    }
+
+    private static boolean isOpeningBracket(char ch) {
+        for (char[] pair : brackets) {
+            if (pair[0] == ch) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static boolean isMatchingPairOfBrackets(char openingBracket, char closingBracket) {
-        return (openingBracket == '(' && closingBracket == ')')
-                || (openingBracket == '[' && closingBracket == ']')
-                || (openingBracket == '{' && closingBracket == '}');
+        for (char[] pair : brackets) {
+            if (pair[0] == openingBracket) {
+                return pair[1] == closingBracket;
+            }
+        }
+        return false;
     }
 }
