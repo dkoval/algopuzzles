e1665e44db274782e4b50de9f0fee19503c4cc3c
dkoval <koval.dmitriy@gmail.com>
2018-06-08 19:38:30 +0200

    added solution to Codefight's `restoreBinaryTree` https://codefights.com/interview-practice/task/AaWaYxi8gjtbqgp2M

---
+++ codefights/src/main/java/com/github/dkoval/algopuzzles/codefights/RestoreBinaryTree.java
@@ -0,0 +1,72 @@
+package com.github.dkoval.algopuzzles.codefights;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+/**
+ * <a href="https://codefights.com/interview-practice/task/AaWaYxi8gjtbqgp2M">restoreBinaryTree</a>
+ * <p>
+ * Given the inorder and preorder traversals of a binary tree t, but not t itself, restore t and return it.
+ */
+public class RestoreBinaryTree {
+
+    static class Tree<T> {
+        T value;
+        Tree<T> left;
+        Tree<T> right;
+
+        Tree(T value, Tree<T> left, Tree<T> right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+
+        Tree(T value) {
+            this.value = value;
+        }
+
+        static boolean equalTrees(Tree<Integer> t1, Tree<Integer> t2) {
+            if (t1 == null && t2 == null) {
+                return true;
+            }
+            if (t1 == null || t2 == null) {
+                return false;
+            }
+            return Objects.equals(t1.value, t2.value)
+                    && equalTrees(t1.left, t2.left)
+                    && equalTrees(t1.right, t2.right);
+        }
+
+        @Override
+        public String toString() {
+            return "Tree{" +
+                    "value=" + value +
+                    ", left=" + (left != null ? left.value + "..." : "null") +
+                    ", right=" + (right != null ? right.value + "..." : "null") +
+                    '}';
+        }
+    }
+
+    static Tree<Integer> restoreBinaryTree(int[] inorder, int[] preorder) {
+        if (inorder.length == 0) {
+            return null;
+        }
+
+        int root = preorder[0];
+        Tree<Integer> node = new Tree<>(root);
+        int leftSubtreeLength = IntStream.range(0, inorder.length)
+                .filter(i -> inorder[i] == root)
+                .findFirst().orElse(-1);
+
+        node.left = restoreBinaryTree(
+                Arrays.copyOfRange(inorder, 0, leftSubtreeLength),
+                Arrays.copyOfRange(preorder, 1, leftSubtreeLength + 1));
+
+        node.right = restoreBinaryTree(
+                Arrays.copyOfRange(inorder, leftSubtreeLength + 1, inorder.length),
+                Arrays.copyOfRange(preorder, leftSubtreeLength + 1, inorder.length));
+
+        return node;
+    }
+}
---
+++ codefights/src/test/java/com/github/dkoval/algopuzzles/codefights/RestoreBinaryTreeTest.java
@@ -0,0 +1,48 @@
+package com.github.dkoval.algopuzzles.codefights;
+
+import com.github.dkoval.algopuzzles.codefights.RestoreBinaryTree.Tree;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.github.dkoval.algopuzzles.codefights.RestoreBinaryTree.Tree.equalTrees;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class RestoreBinaryTreeTest {
+    private final int[] inorder;
+    private final int[] preorder;
+    private final Tree<Integer> expected;
+
+    public RestoreBinaryTreeTest(int[] inorder, int[] preorder, Tree<Integer> expected) {
+        this.inorder = inorder;
+        this.preorder = preorder;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {
+                        new int[]{4, 2, 1, 5, 3, 6},
+                        new int[]{1, 2, 4, 3, 5, 6},
+                        new Tree<>(1,
+                                new Tree<>(2,
+                                        new Tree<>(4),
+                                        null),
+                                new Tree<>(3,
+                                        new Tree<>(5),
+                                        new Tree<>(6)))
+                }
+        });
+    }
+
+    @Test
+    public void restoreBinaryTree() {
+        Tree<Integer> actual = RestoreBinaryTree.restoreBinaryTree(inorder, preorder);
+        assertTrue(equalTrees(actual, expected));
+    }
+}
